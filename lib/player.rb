75d8155f9435e17f2ee025a1593c4dd96a94d4dc
Luis Novoa <luis.matte.novoa@gmail.com>
2019-12-03 12:50:28 -0400

    Fixes rubocop errors

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Player
-    attr_reader :name
-    attr_accessor :sign, :move
-    def initialize(name)
-        @name = name
-    end
+  attr_reader :name
+  attr_accessor :sign, :move
+  def initialize(name)
+    @name = name
+  end
 end
