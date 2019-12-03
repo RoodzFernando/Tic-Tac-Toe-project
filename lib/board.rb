5f5575ea5f4b41f6154ec30d2872bdf1a0033ede
Luis Novoa <luis.matte.novoa@gmail.com>
2019-12-03 17:18:28 -0400

    Moves #board_show to the main file

--- lib/board.rb
+++ lib/board.rb
@@ -6,21 +6,11 @@ class Board
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  def board_show
-    puts '+---+---+---+'
-    puts "| #{@board[0]} | #{@board[1]} | #{@board[2]} |"
-    puts '+---+---+---+'
-    puts "| #{@board[3]} | #{@board[4]} | #{@board[5]} |"
-    puts '+---+---+---+'
-    puts "| #{@board[6]} | #{@board[7]} | #{@board[8]} |"
-    puts '+---+---+---+'
-  end
-
   def board_update(input, index)
-    @board[index] = input
+    board[index] = input
   end
 
   def board_reader(position)
-    @board[position]
+    board[position]
   end
 end
