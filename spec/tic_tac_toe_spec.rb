c111f0b68f63ad486847397e0887a10871e00d91
Luis Novoa <luis.matte.novoa@gmail.com>
2019-12-05 16:16:25 -0400

    Creates test for board_show

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
-# require 'bin/main'
-require './lib/player'
+load File.expand_path('../bin/main', __dir__)
+require File.expand_path('../lib/player', __dir__)
+require File.expand_path('../lib/board', __dir__)
 
 RSpec.describe do
+  describe 'board_show' do
+    it 'shows the board' do
+      board_test = Board.new
+
+      expect { board_show(board_test) }.to output(
+        "+---+---+---+\n"\
+        "| #{board_test.board_reader(0)} | #{board_test.board_reader(1)} | #{board_test.board_reader(2)} |\n"\
+        "+---+---+---+\n"\
+        "| #{board_test.board_reader(3)} | #{board_test.board_reader(4)} | #{board_test.board_reader(5)} |\n"\
+        "+---+---+---+\n"\
+        "| #{board_test.board_reader(6)} | #{board_test.board_reader(7)} | #{board_test.board_reader(8)} |\n"\
+        "+---+---+---+\n"
+      ).to_stdout
+    end
+  end
+
   describe Player do
     describe '#player_signs' do
       it "Check if the player sign is 'X' or 'O':" do
