f5079ca53d6802d402298c80cd4f16df85a2b173
RoodzFernando <fleurantrfernando@gmail.com>
2019-12-06 16:31:33 -0500

    ractoting the variables  into dependent variables

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -6,9 +6,8 @@ require File.expand_path('../lib/board', __dir__)
 
 RSpec.describe do
   describe 'board_show' do
+    let(:board_test) { Board.new }
     it 'shows the board' do
-      board_test = Board.new
-
       expect { board_show(board_test) }.to output(
         "+---+---+---+\n"\
         "| #{board_test.board_reader(0)} | #{board_test.board_reader(1)} | #{board_test.board_reader(2)} |\n"\
@@ -22,50 +21,46 @@ RSpec.describe do
   end
 
   describe 'determine_winner' do
-    it "tells when player 1 wins" do
+    it 'tells when player 1 wins' do
       $player_1 = Player.new('Player1')
       $player_1.sign = 'X'
       $winner = 'X'
-      expect {determine_winner}.to output{
-        "Congratulations! Player1 wins! Want to play again? (Y or N)"
-    }.to_stdout
+      expect { determine_winner }.to output {
+                                       'Congratulations! Player1 wins! Want to play again? (Y or N)'
+                                     }.to_stdout
     end
 
-    it "tells when player 2 wins" do
+    it 'tells when player 2 wins' do
       $player_2 = Player.new('Player2')
       $player_2.sign = 'X'
       $winner = 'X'
-      expect {determine_winner}.to output{
-        "Congratulations! Player2 wins! Want to play again? (Y or N)"
-    }.to_stdout
+      expect { determine_winner }.to output {
+                                       'Congratulations! Player2 wins! Want to play again? (Y or N)'
+                                     }.to_stdout
     end
-  end
 
-  describe 'player_name' do
-    it "creates new players" do
-      name1 = double("player1")
-      expect($player_1.name).to eq('Test1')
-      # expect($player_2).to be_truthy
+    it "tells when it's a draw" do
     end
   end
 
   describe 'determine_winner' do
     it 'Check for the possible end of the game' do
-            player = Player.new("name")
-            player.sign = /X || O/
+      player = Player.new('name')
+      player.sign = /X || O/
       expect($winner == player.sign).to eq(false)
     end
   end
 
   describe 'game_finish?' do
-    it "Check if the game is finish by a win" do
-      win =false
-      win_board = [[0,1,2],[3,4,5],[6,7,9],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    let(:win_board) { [[0, 1, 2], [3, 4, 5], [6, 7, 9], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]] }
+    it 'Check if the game is finish by a win' do
+      win = false
       win_board.each do |elem|
-        elem.each { |n| n = 'X'
-        win = true if n == 'X'
-        expect(win).to eq(true)
-      }
+        elem.each do |n|
+          n = 'X'
+          win = true if n == 'X'
+          expect(win).to eq(true)
+        end
       end
     end
   end
@@ -73,10 +68,9 @@ RSpec.describe do
   describe Player do
     describe '#player_signs' do
       it "Check if the player sign is 'X' or 'O':" do
-        player_sign = Player.new("")
+        player_sign = Player.new('')
         allow(player_sign).to receive(:player_signs).and_return(/X || O/)
       end
     end
   end
 end
-
